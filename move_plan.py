be785aa1b7a21c343ce36bae8a2be95811796bc2
alecgtx <alecgtx@gmail.com>
2021-11-30 11:46:47 +0900

    tello

---
+++ move_plan.py
@@ -0,0 +1,21 @@
+from djitellopy import Tello
+
+tello=Tello()
+
+tello.connect()
+
+tello.takeoff()
+
+#move
+tello.move_up(100)
+#tello.move_forward(50)
+tello.rotate_clockwise(90)
+
+
+
+#tello.move_back(50)
+#tello.move_up(50)
+
+tello.land()
+
+pass
