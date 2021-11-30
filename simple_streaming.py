e79b7f9170533cf9452ff501a2d7d589fe3e8810
alecgtx <alecgtx@gmail.com>
2021-11-30 12:40:48 +0900

    stream

---
+++ simple_streaming.py
@@ -0,0 +1,18 @@
+from djitellopy import Tello
+
+tello=Tello()
+tello.connect()
+
+tello.streamon()
+frame_read=tello.get_frame_read()
+
+import cv2
+while True:
+    cv2.imshow('tello stream', frame_read.frame)
+    key=cv2.waitKey(1)
+    if key==ord('q'):
+        break
+    
+tello.streamoff()
+
+pass
